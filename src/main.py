605f55e98d01907f2fc1e9024fc2973ed39fe407
Yenuli0808 <yasyenuli@gmail.com>
2026-03-11 02:58:52 +0530

    add a logging system to record every pipeline step

--- src/main.py
+++ src/main.py
@@ -2,20 +2,26 @@ import os
 from ingest import ingest_data
 from transform import transform_data
 from load import load_data
+from logger import setup_logger
 
 def run_pipeline():
+    logger = setup_logger()
+    logger.info("Pipeline started")
 
     raw_file_path = os.path.join("..","Dataset", "superstore.csv")
     processed_file_path = os.path.join("..","Dataset", "processed_superstore.csv")
 
     # Step 1: Ingest
     df = ingest_data(raw_file_path)
+    logger.info("Data ingestion completed")
 
     # Step 2: Transform
     df_transformed = transform_data(df)
+    logger.info("Transformation completed")
 
     # Step 3: Load
     load_data(df_transformed, processed_file_path)
+    logger.info("Loading completed")
 
     print("🚀 Pipeline executed successfully!")
 
